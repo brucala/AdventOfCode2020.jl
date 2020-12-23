0b4577fda19baa6f1e05acbac5a5697ea937ddcf
brucala <brucala@gmail.com>
2020-12-23 17:20:31 +0100

    unify solutions

--- src/day23.jl
+++ src/day23.jl
@@ -4,44 +4,8 @@ using .Utils
 
 export solve1, solve2
 
-popfirstN!(x, n) = Tuple(popfirst!(x) for i in 1:n)
-
 minus1(n, N) = (N+n-2) % N + 1
 plus1(n, N) = n % N + 1
-
-function move!(x, N)
-    current, next = popfirst!(x), popfirstN!(x, 3)
-    destination = minus1(current, N)
-    while destination ∈ next
-        destination = minus1(destination, N)
-    end
-    idestination = plus1(find(x, destination), N)
-    splice!(x, idestination:idestination-1, next)
-    push!(x, current)
-end
-
-function parse_input(x)
-    collect(x) .|> toint
-end
-
-function sol(x)
-    i = find(x, 1)
-    return join([x[i+1:end]; x[1:i-1]]) |> toint
-end
-
-function sol2(x)
-    i = find(x, 1)
-    return x[i+1] * x[i+2]
-end
-
-function solve1(x, n=100)
-    cups = parse_input(x)
-    N = length(cups)
-    for i in 1:n
-        move!(cups, N)
-    end
-    return sol(cups)
-end
 
 mutable struct Cup
     label::Int
@@ -91,10 +55,15 @@ function move!(cups::Vector{Cup}, cup::Cup)
     return cup.next
 end
 
-sol(cups::Vector{Cup}) = label.(Iterators.take(cups[1].next, 8)) |> join |> toint
+sol1(cups::Vector{Cup}) = label.(Iterators.take(cups[1].next, 8)) |> join |> toint
 sol2(cups::Vector{Cup}) = prod(label.(Iterators.take(cups[1].next, 2)))
 
-function solve2(x, nmoves=10_000_000, ncups=1_000_000)
+function parse_input(x)
+    collect(x) .|> toint
+end
+
+
+function solve(x, nmoves, ncups, fsol)
     labels = parse_input(x)
     cups = arrange_cups(labels, ncups)
     cup = cups[labels[1]]
@@ -102,7 +71,10 @@ function solve2(x, nmoves=10_000_000, ncups=1_000_000)
         cup = move!(cups, cup)
     end
     #return cups
-    return sol2(cups)
+    return fsol(cups)
 end
+
+solve1(x, nmoves=100) = solve(x, nmoves, 0, sol1)
+solve2(x) = solve(x, 10_000_000, 1_000_000, sol2)
 
 end  # module
