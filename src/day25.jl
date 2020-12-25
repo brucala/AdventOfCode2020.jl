805d4e4aa066b501f70ec4a168b718951a6bc197
brucala <brucala@gmail.com>
2020-12-25 13:46:04 +0100

    Day 25 solution with iterator interface

--- src/day25.jl
+++ src/day25.jl
@@ -6,19 +6,45 @@ export solve1
 
 parse_input(x) = read_ints(x)
 
-function transform(subject_number, loop_size=20201227; find_size=-1)
+const MOD = 20201227
+
+# Nice solution that to learn the use of the iterator interface
+struct Powers
+    number::Int
+    count::Int
+end
+Powers(number) = Powers(number, MOD)
+Base.length(p::Powers) = p.count
+Base.iterate(p::Powers, state=(1, 1)) = (pow = state[2] * p.number % MOD; state[1] > p.count ? nothing : (pow, (state[1]+1, pow)))
+Base.keys(p::Powers) = 1:length(p)
+last(p) = first(Iterators.drop(p, length(p)-1))
+#= Alternative to last: define indexing and lastindex
+Base.lastindex(p::Powers) = length(p)
+function Base.getindex(p::Powers, i::Int)
+    for (n,pow) in enumerate(p)
+        n == i && return pow
+    end
+end
+=#
+
+transform(subject_number, loop_size) = last(Powers(subject_number, loop_size))
+find_loopsize(subject_number, pubkey) = findfirst(==(pubkey), Powers(subject_number))
+
+function solve1(x)
+    card_pubkey, door_pubkey = parse_input(x)
+    card_loop_size = find_loopsize(7, card_pubkey)
+    encryption_key = transform(door_pubkey, card_loop_size)
+end
+
+# Alternative solution
+function transform_alt(subject_number, loop_size=MOD; find_size=-1)
     i = value = 1
     while i < loop_size && value != find_size
-        value = (value * subject_number) % 20201227
+        value = (value * subject_number) % MOD
         i += 1
     end
     return value == find_size ? i : value
 end
-
-function solve1(x)
-    card_pubkey, door_pubkey = parse_input(x)
-    card_loop_size = transform(7; find_size=card_pubkey)
-    encryption_key = transform(door_pubkey, card_loop_size)
-end
+find_loopsize_alt(subject_number, pubkey) = transform_alt(subject_number; find_size=pubkey)
 
 end  # module
