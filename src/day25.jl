a8c054c27954934f92c9086e038ba897641bf06c
brucala <brucala@gmail.com>
2020-12-25 18:02:28 +0100

    day 25: boost by using powermod

--- src/day25.jl
+++ src/day25.jl
@@ -15,16 +15,13 @@ struct Powers
 end
 Powers(number) = Powers(number, MOD)
 Base.length(p::Powers) = p.count
+# faster iteration than using powermod -> faster find_loopsize
 Base.iterate(p::Powers, state=(1, 1)) = (pow = state[2] * p.number % MOD; state[1] > p.count ? nothing : (pow, (state[1]+1, pow)))
 Base.keys(p::Powers) = 1:length(p)
-last(p) = first(Iterators.drop(p, length(p)-1))
-#= Alternative to last: define indexing and lastindex
 Base.lastindex(p::Powers) = length(p)
-function Base.getindex(p::Powers, i::Int)
-    for (n,pow) in enumerate(p)
-        n == i && return pow
-    end
-end
+Base.getindex(p::Powers, i::Int) = powermod(p.number, i, MOD)
+#= Alternative to lastindex and getinde: does the full iteration
+last(p) = first(Iterators.drop(p, length(p)-1))
 =#
 
 transform(subject_number, loop_size) = last(Powers(subject_number, loop_size))
@@ -36,15 +33,20 @@ function solve1(x)
     encryption_key = transform(door_pubkey, card_loop_size)
 end
 
-# Alternative solution
-function transform_alt(subject_number, loop_size=MOD; find_size=-1)
-    i = value = 1
-    while i < loop_size && value != find_size
+# Alternative solution (few ms faster)
+transform_alt(subject_number, loop_size=MOD) = powermod(subject_number, loop_size, MOD)
+function find_loopsize_alt(subject_number, find_size)
+    value = 1
+    for i in 1:MOD
         value = (value * subject_number) % MOD
-        i += 1
+        value == find_size && return i
     end
-    return value == find_size ? i : value
 end
-find_loopsize_alt(subject_number, pubkey) = transform_alt(subject_number; find_size=pubkey)
+
+function solve1_alt(x)
+    card_pubkey, door_pubkey = parse_input(x)
+    card_loop_size = find_loopsize_alt(7, card_pubkey)
+    encryption_key = transform_alt(door_pubkey, card_loop_size)
+end
 
 end  # module
